84bc3517c9e5451bb6b937816c078560c7395101
DejaMeshae <daemeshae@yahoo.com>
2019-05-13 13:33:29 -0500

    Created Jobs model

---
+++ DejasList/Models/Jobs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DejasList.Models
+{
+    public class Jobs
+    {
+        [Key]
+        public int JobsId { get; set; }
+    }
+}
