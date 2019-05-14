a5f8e6a84e3ddc04b4649b11de433e4e32cc3cd1
Charles Erickson <cse621@gmail.com>
2019-05-14 11:48:55 -0500

    Built client view

--- DejasList/Models/Client.cs
+++ DejasList/Models/Client.cs
@@ -15,6 +15,7 @@ namespace DejasList.Models
         public string LastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
+        public string State { get; set; }
         public string Zipcode { get; set; }
         public string Email { get; set; }
 
