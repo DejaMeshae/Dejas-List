485f9040dd09242cbc8b5670f55f4f5ab32d9aed
Charles Erickson <cse621@gmail.com>
2019-05-14 13:26:58 -0500

    Added jobs controller and jobs view

---
+++ DejasList/Controllers/JobsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DejasList.Models;
+
+namespace DejasList.Controllers
+{
+    public class JobsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            var jobs = db.Jobs.Include(j => j.Client).Include(j => j.Contractor);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName");
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "FirstName");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "JobsId,TypeOfProject,SizeOfProject,Budget,ClientId,ContractorId")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(jobs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", jobs.ClientId);
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "FirstName", jobs.ContractorId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", jobs.ClientId);
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "FirstName", jobs.ContractorId);
+            return View(jobs);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "JobsId,TypeOfProject,SizeOfProject,Budget,ClientId,ContractorId")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", jobs.ClientId);
+            ViewBag.ContractorId = new SelectList(db.Contractors, "ContractorId", "FirstName", jobs.ContractorId);
+            return View(jobs);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Jobs.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jobs jobs = db.Jobs.Find(id);
+            db.Jobs.Remove(jobs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
