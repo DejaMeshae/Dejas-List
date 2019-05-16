6e2200441648551536935202c24aea4a91f479d8
Charles Erickson <cse621@gmail.com>
2019-05-16 08:53:22 -0500

    pulling newest project

--- DejasList/EmailController.cs
+++ DejasList/EmailController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Threading.Tasks;
+using DejasList.Models;
 
 
 
@@ -15,23 +16,25 @@ namespace DejasList
     public class EmailController : ApiController
     {
 
-        private static void Main()
-        {
-            Execute().Wait();
-        }
+        //private static void Main()
+        //{
+        //    Execute().Wait();
+        //}
 
-        static async Task Execute()
-        {
-            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-        }
+        //static async Task Execute()
+        //{
+        //    var UserLoggedIn= User.Identity.GetUserId();
+        //    var Email = UserLoggedIn.Email;
+        //    var apiKey = Environment.GetEnvironmentVariable(APIKeys.SendGridKey);
+        //    var client = new SendGridClient(apiKey);
+        //    var from = new EmailAddress(Email, "Example User");
+        //    var subject = "Sending with SendGrid is Fun";
+        //    var to = new EmailAddress("test@example.com", "Example User");
+        //    var plainTextContent = "and easy to do anywhere, even with C#";
+        //    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+        //    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        //    var response = await client.SendEmailAsync(msg);
+        //}
 
 
 
